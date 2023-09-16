5d685c95273020cbfa7554e441668a4d4f06d13c
bakmaiev <thisyoungrhyme@gmail.com>
2023-09-17 01:02:14 +0300

    Fix mistakes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,8 +14,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/favorites" element={<Favorites />} />
-          <Route path="*" element={<Home />} />
         </Route>
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
